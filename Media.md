c4a43d6db4e8a5c215af80ab5e6abe5301d9e0f7
esoo27 <esoo27@naver.com>
2021-06-13 17:49:31 +0900

    Media

---
+++ Media.md
@@ -0,0 +1,13 @@
+Media
+=====
+Image
+-----
+>### NightSkyCity.jpg = https://pxhere.com/ko/photo/1337271
+>##### CC0 Public Domain
+>##### Free for personal and commercial use
+>##### No attributes required
+
+>### DisasterFire.jpg = https://pixabay.com/ko/photos/%ED%99%94%EC%9E%AC-%EC%97%B4-%ED%95%98%EC%9A%B0%EC%8A%A4-%EC%9E%A5%EC%9E%91-5666910/
+>##### Pixabay License
+>##### 상업적 용도로 사용 가능
+>##### 출처 안 밝혀도 됨
---
+++ MediaVariable.js
@@ -0,0 +1,19 @@
+// Name       : Jonghyeon Lee
+// Assignment : make_a_game
+// Course     : CS099
+// Spring 2021
+
+let NightCity;
+let FireDisaster;
+let HandL;
+let HandR;
+let ChatBox;
+
+function preload()
+{
+    NightCity = loadImage('assets/NightSkyCity.jpg');
+    FireDisaster = loadImage('assets/DisasterFire.jpg');
+    HandL = loadImage('assets/Hand.png');
+    HandR = loadImage('assets/Hand2.png');
+    ChatBox = loadImage('assets/ChatBox.png');
+}
