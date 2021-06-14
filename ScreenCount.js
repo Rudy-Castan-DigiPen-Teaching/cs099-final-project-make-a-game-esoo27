70c00163f44e641b9c364424feae669740decf7e
esoo27 <esoo27@naver.com>
2021-06-15 05:32:06 +0900

    ScreenCount

---
+++ ScreenCount.js
@@ -0,0 +1,36 @@
+// Name       : Jonghyeon Lee
+// Assignment : make_a_game
+// Course     : CS099
+// Spring 2021
+
+const MAIN_MENU = 0;
+const HOW_TO_PLAY = 1;
+const SCENE_1 = 2;
+const SCENE_2 = 3;
+const STAGE_1 = 4;
+const STAGE_2 = 5;
+const STAGE_3 = 6;
+const EPILOGUE = 7;
+
+const S1_1F_Map1 = 0;
+const S1_1F_Map2 = 1;
+const S1_1F_Map3 = 2;
+const S1_1F_Map4 = 3;
+const S1_1F_Map5 = 4;
+const S1_1F_Map6 = 5;
+const S1_1F_Map7 = 6;
+const S1_1F_Map8 = 7;
+const S1_1F_Stairs = 8;
+const S1_2F_Stairs = 9;
+const S1_2F_Map1 = 10;
+const S1_2F_Map2 = 11;
+const S1_2F_Map3 = 12;
+const S1_2F_Map4 = 13;
+const S1_2F_Map5 = 14;
+const S1_2F_Map6 = 15;
+const S1_2F_Map7 = 16;
+const S1_Out = 17;
+const S1_Result = 18;
+
+let ScenesNumbers = MAIN_MENU;
+let S1_Maps = S1_Out;
