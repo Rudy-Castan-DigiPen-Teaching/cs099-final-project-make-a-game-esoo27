a3dad31439b1129f8b421a74a9a32603e1d7abd8
esoo27 <esoo27@naver.com>
2021-06-22 00:56:55 +0900

    MediaVar

--- MediaVariable.js
+++ MediaVariable.js
@@ -8,6 +8,28 @@ let FireDisaster;
 let HandL;
 let HandR;
 let ChatBox;
+let PeopleSearch;
+let AnswerButton;
+let BurningHouseView;
+let BurningHouse1;
+let BurningHouse2;
+let BurningHouse3;
+let BurningHouse4;
+let PictureFrame;
+let FootPrint;
+let IgnitionPoint;
+let DivorceDocument;
+let Picture;
+let Knife;
+let S1RPerson1;
+let S1RPerson2;
+let S1RPerson3;
+let BuildingCollapse;
+let CollapseBuilding1;
+let CollapseBuilding2;
+let CollapseBuilding3;
+let CollapseBuilding4;
+let CollapseBuildingOut;
 
 function preload()
 {
@@ -16,4 +38,26 @@ function preload()
     HandL = loadImage('assets/Hand.png');
     HandR = loadImage('assets/Hand2.png');
     ChatBox = loadImage('assets/ChatBox.png');
+    PeopleSearch = loadImage('assets/PeopleSearch.png');
+    BurningHouseView = loadImage('assets/BurningHouseView.jpg');
+    BurningHouse1 = loadImage('assets/BurningHouse1.jpg');
+    BurningHouse2 = loadImage('assets/BurningHouse2.jpg');
+    BurningHouse3 = loadImage('assets/BurningHouse3.jpg');
+    BurningHouse4 = loadImage('assets/BurningHouse4.jpg');
+    PictureFrame = loadImage('assets/S1Object1.png');
+    FootPrint = loadImage('assets/S1Object2.png');
+    IgnitionPoint = loadImage('assets/S1Object3.png');
+    DivorceDocument = loadImage('assets/S1Object4.png');
+    Picture = loadImage('assets/S1Object5.png');
+    Knife = loadImage('assets/S1Object6.png');
+    S1RPerson1 = loadImage('assets/S1RescuePerson1.png');
+    S1RPerson2 = loadImage('assets/S1RescuePerson2.png');
+    S1RPerson3 = loadImage('assets/S1RescuePerson3.png');
+    AnswerButton = loadImage('assets/AnswerButton.png');
+    BuildingCollapse = loadImage('assets/BuildingCollapse.jpg');
+    CollapseBuilding1 = loadImage('assets/CollapseBuilding1.jpg');
+    CollapseBuilding2 = loadImage('assets/CollapseBuilding2.jpg');
+    CollapseBuilding3 = loadImage('assets/CollapseBuilding3.jpg');
+    CollapseBuilding4 = loadImage('assets/CollapseBuilding4.jpg');
+    CollapseBuildingOut = loadImage('assets/CollapseBuildingOut.jpg');
 }
