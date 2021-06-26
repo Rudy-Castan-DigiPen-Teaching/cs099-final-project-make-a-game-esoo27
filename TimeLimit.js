53d76f0647642f2b17ba3eaac8f59fdb22e2c819
esoo27 <esoo27@naver.com>
2021-06-26 20:51:05 +0900

    TimeLimit Update

--- TimeLimit.js
+++ TimeLimit.js
@@ -3,9 +3,12 @@
 // Course     : CS099
 // Spring 2021
 
-let Time_limitS1 = 300;
-let Time_limitS2 = 400;
-let Time_limitS3 = 600;
+//Time Limit
+
+let Time_limitS1 = 400;
+let Time_limitS2 = 500;
+let Time_limitS3 = 900;
+let Time_limitS4 = 999;
 
 function Limit_setS1(x, y)
 {
@@ -27,6 +30,8 @@ function Limit_setS1(x, y)
         fill('red');
         text("GAME OVER", width/2, height/2);
         pop();
+
+        Time_limitS1 = 0;
     }
     push();
     textSize(20);
@@ -56,6 +61,8 @@ function Limit_setS2(x, y)
         fill('red');
         text("GAME OVER", width/2, height/2);
         pop();
+
+        Time_limitS2 = 0;
     }
     push();
     textSize(20);
@@ -85,11 +92,44 @@ function Limit_setS3(x, y)
         fill('red');
         text("GAME OVER", width/2, height/2);
         pop();
+
+        Time_limitS3 = 0;
     }
     push();
     textSize(20);
     fill('red');
     text(Time_limitS3, x, y);
+    text("Limit:", x - 50, y);
+    pop();
+}
+
+function Limit_setS4(x, y)
+{
+    if(frameCount % 60 == 0 && Time_limitS4 > 0)
+    {
+        Time_limitS4--;
+    }
+    if(Time_limitS4 <= 0)
+    {
+        push();
+        fill('white');
+        rectMode(CENTER);
+        rect(width/2, height/2, width, height);
+        pop();
+
+        push();
+        textAlign(CENTER, CENTER);
+        textSize(50);
+        fill('cyan');
+        text("FREE WILL", width/2, height/2);
+        pop();
+
+        Time_limitS4 = 0;
+    }
+    push();
+    textSize(20);
+    fill('red');
+    text(Time_limitS4, x, y);
     text("Limit:", x - 50, y);
     pop();
 }
